f66ee8ab085f135acb877b387420595a82c9e4b8
eddy <cumtsjh@163.com>
2026-07-23 15:02:39 +0800

    tj

--- locale-redirect.js
+++ locale-redirect.js
@@ -27,6 +27,13 @@
 
     const suffix = path === "/" ? "" : path.slice(1);
     window.location.replace(`${localePath}${suffix}${window.location.search}${window.location.hash}`);
+
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xqtr731p4c");
+
   } catch (error) {
     console.warn(error);
   }
